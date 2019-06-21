064762b70d101a5bc0a6e2d06f6f4b067199ea9e
gyjddtang <805793774@qq.com>
2019-06-21 14:14:30 +0800

    css mask

--- ts/enum3.js
+++ ts/enum3.js
@@ -1,5 +1,14 @@
 /**
  * Created by MiliGao on 2018/1/2.
  */
-var directions = [0 /* up */, 1 /* down */, 2 /* left */, 3 /* right */];
+// 当访问枚举值时，为了避免生成多余的代码和间接引用，可以使用常数枚举。
+// 常数枚举不可能有计算成员。
+var Direction;
+(function (Direction) {
+    Direction["up"] = "1";
+    Direction["down"] = "2";
+    Direction["left"] = "3";
+    Direction["right"] = "4";
+})(Direction || (Direction = {}));
+var directions = [Direction.up, Direction['2'], Direction.left, Direction.right];
 console.log(directions);
--- ts/enum3.ts
+++ ts/enum3.ts
@@ -5,12 +5,12 @@
 // 当访问枚举值时，为了避免生成多余的代码和间接引用，可以使用常数枚举。
 // 常数枚举不可能有计算成员。
 
-const enum Direction {
-  up,
-  down,
-  left,
-  right
+enum Direction {
+  up = '1',
+  down = '2',
+  left = '3',
+  right = '4'
 }
 
-let directions = [Direction.up, Direction.down, Direction.left, Direction.right];
+let directions = [Direction.up, Direction['2'], Direction.left, Direction.right];
 console.log(directions);
